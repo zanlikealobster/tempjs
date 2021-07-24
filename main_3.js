4fa241671082096773c55bcdded8298323c16a41
zanlikealobster <64977470+zanlikealobster@users.noreply.github.com>
2021-07-24 13:43:09 +0200

    Update and rename main_.js to main_3.js

---
+++ main_3.js
@@ -0,0 +1,25 @@
+import "./vue.js"
+
+const {createApp} = Vue;
+const {ref} = Vue;
+
+const app = {
+  name: 'Test',
+
+  setup() {
+      let title = ref(1);
+      
+      return {
+        title
+      };
+  },
+  
+  template: `
+    <div>
+    <button v-on:click="title++">+</button>
+      <h1>{{title}}</h1>
+    </div>
+  `,
+};
+createApp(app).mount('#app');
+
